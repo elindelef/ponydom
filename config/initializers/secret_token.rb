3e1f0dcf7a3c29e229a0a157af2fccc5b300d881
Elaine Lindelef <eel@cognitivity.com>
2013-04-01 11:28:31 -0700

    fresh rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ponydom::Application.config.secret_token = '700294ebdaf6f59f68ce50eddc98d1d28b0c2740f516574a444be4241f1b42aefe68e5ce3f6336da360dd25d7ec6ca6dedbaa064a2574d0efa11e109d30f32ff'
